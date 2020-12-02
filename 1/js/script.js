4a8c73552aba6154c29eef76fee9a12d01bbee1f
ASA-GH <samorodov21@gmail.com>
2020-12-03 02:01:34 +0700

    Generate random array

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,134 +1,155 @@
-const inputLength = document.getElementById("inputLength");
-const inputLimit = document.getElementById("inputLimit");
-const buttonStart = document.getElementById("buttonStart");
+let inputLimit = document.getElementById("inputLimit");
+let inputLength = document.getElementById("inputLength");
 
-const include = (url) => {
-    let script = document.createElement('script');
-    script.src = url;
-    document.getElementsByTagName('body')[0].appendChild(script);
-    return script;
-}
-
-const past = (str, sentences) => {
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    let parent = document.getElementById("wrapproduct");
-    let div = document.createElement("div");
-    div.className = "product"
-    div.innerHTML = `<h4>${parent.childElementCount + 1}. ${str.replace(/^./, s => s.toUpperCase())}</h4><p>${sentences[getRandomInt(0, (sentences.length - 1))]}</p>`
-    parent.append(div);
-
-    let count = 63;
-    let progress = document.getElementById("progress");
-    progress.innerHTML = `Progress: ${Math.round((parent.childElementCount.toFixed(2) / count.toFixed(2) * 100.00))} of 100`;
-
-    return 'success'
-}
-
-const getStrings1 = (text) => {
-    return text.replace(/"+/g, '').split('\n')
+const getText = (text, pattern) => {
+  return text.match(pattern);
 };
 
-const getSentences1 = (text) => {
-    let arr = getStrings1(text);
-    let text1 = arr.join(' ')
-    let text2 = text1.match(/([^\.!\?]+[\.!\?]+["']?)|\s*$/g);
-    text2.pop()
-    return text2;
+const buttonStartDisabled = () => {
+  buttonStart.disabled = !inputLength.value || !inputLimit.value;
 };
 
-const start = () => {
-    let s = include("./js/text.js")
-    s.onload = function() {
-        setTimeout(function() {
-            queue(text, past, 10);
-        }, 0);
-    };
-}
-
-const getText = function(text, pattern) {
-    return text.match(pattern);
+const inputLengthHandler = (e) => {
+  e.target.value = getText(e.target.value, /^([1-9][0-9]?|100)$/g);
+  buttonStartDisabled();
+  return e;
 };
 
-const inputLengthHandler = function(e) {
-    e.target.value = getText(e.target.value, /^([1-9][0-9]?|100)$/g);
-    buttonStartDisabled();
-    return e;
-};
-const inputLimitHandler = function(e) {
-    e.target.value = getText(e.target.value, /^([1-9]|10)$/g);
-    buttonStartDisabled();
-    return e;
-};
-const buttonStartDisabled = function() {
-    buttonStart.disabled = !inputLength.value || !inputLimit.value;
+const inputLimitHandler = (e) => {
+  e.target.value = getText(e.target.value, /^([1-9]|10)$/g);
+  buttonStartDisabled();
+  return e;
 };
 
-function buttonStartHandler() {
-    buttonStart.disabled = true;
-    inputLength.disabled = true;
-    inputLimit.disabled = true;
-}
-
-const queue = (text, f, limit) => {
-    const getPromise = (str, sentences) => {
-        const promise = () =>
-            new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve(f(str, sentences));
-                }, Math.round(Math.random() * 9000) + 1000);
-            });
-
-        return promise();
-    };
-
-    const getStrings = (text) => {
-        return text.replace(/"+/g, '').split('\n')
-    };
-
-    const getSentences = (text) => {
-        let arr = getStrings(text);
-        let text1 = arr.join(' ')
-        let text2 = text1.match(/([^\.!\?]+[\.!\?]+["']?)|\s*$/g);
-        text2.pop()
-        return text2;
-    };
-
-    const _next = () => {
-        return running.length < limit && promises.length;
-    };
-
-    const run = () => {
-        while (_next()) {
-            const promise = promises.shift();
-            promise.then((a) => {
-                complete.push(a);
-                running.shift();
-                run();
-            });
-            running.push(promise);
-        }
-    };
-
-    let sentences = getSentences(text);
-    let arrStr = getStrings(text);
-
-    this.promises = [];
-    for (let i in arrStr) {
-        this.promises.push(getPromise(arrStr[i], sentences));
-    }
-    this.total = promises.length;
-    this.running = [];
-    this.complete = [];
-    this.limit = limit;
-
-    run();
-    return this.complete;
+const buttonStartHandler = () => {
+  buttonStart.disabled = true;
+  inputLength.disabled = true;
+  inputLimit.disabled = true;
 };
+
+const enabledHandler = () => {
+    buttonStart.disabled = false;
+    inputLength.disabled = false;
+    inputLimit.disabled = false;
+};
+
 inputLength.addEventListener("input", inputLengthHandler);
 inputLimit.addEventListener("input", inputLimitHandler);
 buttonStart.addEventListener("click", buttonStartHandler);
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getStrings = (text, lengthArr) => {
+  let strings = text.replace(/"+/g, "").match(/(.{10,200})/g);
+
+  let randomStrings = [];
+  for (let i = 0; i < lengthArr; i++) {
+    randomStrings.push(strings[getRandomInt(0, strings.length - 1)].trim());
+  }
+  return randomStrings;
+};
+
+const getSentences = (text) => {
+  let arr = text.replace(/"+/g, "").split("\n");
+  let text1 = arr.join(" ");
+  let text2 = text1.match(/([^\.!\?]+[\.!\?]+["']?)|\s*$/g);
+  text2.pop();
+  return text2;
+};
+
+const past = (str, sentences) => {
+  let parent = document.getElementById("wrapproduct");
+  let div = document.createElement("div");
+  div.className = "product";
+
+  let title = str.trim().replace(/^./, (s) => s.toUpperCase());
+  let sentence = sentences[getRandomInt(0, sentences.length - 1)]
+    .trim()
+    .replace(/^./, (s) => s.toUpperCase());
+
+  div.innerHTML = `<h4>${parent.childElementCount + 1}. ${title}</h4><p>${sentence}</p>`;
+  parent.append(div);
+ 
+  let progress = document.getElementById("progress");
+  progress.innerHTML = `Progress: ${
+    Math.round((parseFloat(parent.childElementCount) / parseFloat(inputLength.value)) * 100.0)
+  } of 100`;
+
+  return "success";
+};
+
+const finised = (running) => {
+  if (!running)
+    enabledHandler();
+};
+
+const include = (url) => {
+  let script = document.createElement("script");
+  script.src = url;
+  document.getElementsByTagName("body")[0].appendChild(script);
+  return script;
+};
+  
+const start = () => {
+  let s = include("./js/text.js");
+  s.onload = function () {
+    setTimeout(function () {
+      let randomStrs = getStrings(text, inputLength.value);
+      let sentences = getSentences(text);
+      queue([randomStrs, sentences, finised], past, inputLimit.value);
+    }, 0);
+  };
+};
+
+const queue = (objects, f, limit) => {
+  const getPromise = (str, sentences) => {
+    const promise = () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(f(str, sentences));
+        }, Math.round(Math.random() * 9000) + 1000);
+      });
+
+    return promise();
+  };
+
+  const _next = () => {
+    return running.length < limit && promises.length;
+  };
+
+  let finised = objects[2];
+
+  const run = () => {
+    while (_next()) {
+      const promise = promises.shift();
+      promise.then((a) => {
+        complete.push(a);
+        running.shift();
+        run();
+        finised(_next())
+      });
+      running.push(promise);
+    }
+  };
+
+  let randomStrs = objects[0];
+  let sentences = objects[1];
+
+  this.promises = [];
+  for (let i in randomStrs) {
+    this.promises.push(getPromise(randomStrs[i], sentences));
+  }
+
+  //this.promises = {..._promises};
+  this.total = promises.length;
+  this.running = [];
+  this.complete = [];
+  this.limit = limit;
+
+  run();
+  return this.promises;
+};
