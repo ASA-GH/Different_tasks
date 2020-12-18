230ba5f4605c4b87c0cd06a49decfd0c19c18388
ASA-GH <samorodov21@gmail.com>
2020-12-18 23:20:37 +0700

    Created the project "race".

---
+++ 3/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapperApp">
+      <div className="innerApp"></div>
+    </div>
+  );
+}
+
+export default App;
