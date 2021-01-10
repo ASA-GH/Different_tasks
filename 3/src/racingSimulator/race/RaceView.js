a22fe3b6468ab200299ca2b92ce62bdda46b5555
ASA-GH <samorodov21@gmail.com>
2021-01-11 01:03:14 +0700

    Replaced the dot with pictures.

--- 3/src/racingSimulator/race/RaceView.js
+++ 3/src/racingSimulator/race/RaceView.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
-import img from "../../image/transport.svg";
-import testA from "../transport/transportView/Test";
 import "../../App.scss";
 
 const RaceView = (props) => {
@@ -10,6 +8,9 @@ const RaceView = (props) => {
   let innerRadius = 100;
   let widthSvg = 310;
   let heightSvg = 310;
+
+  let widthUse = 46;
+  let heightUse = 46;
   let angle0 = 0;
 
   const CenterCoordinateX = (widthSvg) => {
@@ -35,12 +36,14 @@ const RaceView = (props) => {
     outerRadius,
     innerRadius,
     widthSvg,
+    widthUse,
     angle0
   ) => {
     let cos = Math.cos(GetAngle(member, distance, angle0));
     let x = CenterCoordinateX(widthSvg);
     let radius = Radius(outerRadius, innerRadius);
-    return x + radius * cos;
+    let xUse = widthUse / 2;
+    return x + radius * cos - xUse;
   };
   const PositionY = (
     member,
@@ -48,103 +51,82 @@ const RaceView = (props) => {
     outerRadius,
     innerRadius,
     heightSvg,
+    heightUse,
     angle0
   ) => {
     let sin = Math.sin(GetAngle(member, distance, angle0));
     let y = CenterCoordinateY(heightSvg);
     let radius = Radius(outerRadius, innerRadius);
-    return y + radius * sin;
+    let yUse = heightUse / 2;
+    return y + radius * sin - yUse;
   };
-
-  // transportSvg
-
-  const MemberView = (member) => {
-    switch (member.type) {
-      case "Bike":
-        return (
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-labelledby="bikeIconTitle"
-            stroke="#2329D6"
-            stroke-width="0.75"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            color="#2329D6"
-          >
-            <title id="bikeIconTitle">Bike</title>
+  let GetDefs = () => {
+    return (
+      <defs>
+        <symbol id="Car" viewBox="0 0 24 24" height="40px" width="40px">
+          <path d="M3 12L5 7H7M3 12H2V17H3M3 12H7M18 12L16 7H12M18 12H20C21.1046 12 22 12.8954 22 14V17H20M18 12H12M7 17H16M7 7V12M7 7H12M7 12H12M12 12V7" />
+          <circle cx="5" cy="17" r="2" />
+          <circle cx="17" cy="17" r="2" />
+        </symbol>
+        <symbol id="Bike" viewBox="0 0 24 24">
+          <g>
             <circle cx="14" cy="6" r="1" />
             <path d="M12 18V14L9 12L12 9L14 11L16 12" />
-            <circle cx="6" cy="17" r="3" /> <circle cx="18" cy="17" r="3" />
-          </svg>
-        );
-      case "Car":
-        return (
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-labelledby="carAltIconTitle"
-            stroke="#2329D6"
-            stroke-width="0.75"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            color="#2329D6"
-          >
-            <title id="carAltIconTitle">Car</title>
-            <path d="M3 12L5 7H7M3 12H2V17H3M3 12H7M18 12L16 7H12M18 12H20C21.1046 12 22 12.8954 22 14V17H20M18 12H12M7 17H16M7 7V12M7 7H12M7 12H12M12 12V7" />
-            <circle cx="5" cy="17" r="2" /> <circle cx="18" cy="17" r="2" />
-          </svg>
-        );
-      case "Truck":
-        return (
-          <svg
-            width="32px"
-            height="32px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-labelledby="transportIconTitle"
-            stroke="#2329D6"
-            stroke-width="0.75"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            color="#2329D6"
-          >
-            <title id="transportIconTitle">Transport</title>
-            <path d="M4 17H2V7H15V17H8" />
-            <path d="M20 17H22V12.5556L20 9H15V17H16" />
-            <circle cx="6" cy="17" r="2" /> <circle cx="18" cy="17" r="2" />
-          </svg>
-        );
-      default:
-        return "Error";
-    }
+            <circle cx="6" cy="17" r="3" />
+            <circle cx="18" cy="17" r="3" />
+          </g>
+        </symbol>
+        <symbol id="Truck" viewBox="0 0 24 24" height="40px" width="40px">
+          <path d="M4 17H2V7H15V17H8" />
+          <path d="M20 17H22V12.5556L20 9H15V17H16" />
+          <circle cx="6" cy="17" r="2" />
+          <circle cx="18" cy="17" r="2" />
+        </symbol>
+      </defs>
+    );
   };
-  let test = (
-    <svg
-      height="20px"
-      id="svg_2"
-      viewBox="0 0 24 24"
-      width="20px"
-      x="2"
-      y="0"
-      fill="#ffffff"
-      stroke="#2329D6"
-      stroke-width="0.75"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      color="#2329D6"
-    >
-      <path d="M3 12L5 7H7M3 12H2V17H3M3 12H7M18 12L16 7H12M18 12H20C21.1046 12 22 12.8954 22 14V17H20M18 12H12M7 17H16M7 7V12M7 7H12M7 12H12M12 12V7" />
-      <circle cx="5" cy="17" r="2" /> <circle cx="17" cy="17" r="2" />
-    </svg>
-  );
+  let GetUse = (
+    member,
+    distance,
+    outerRadius,
+    innerRadius,
+    widthSvg,
+    widthUse,
+    heightSvg,
+    heightUse,
+    angle0
+  ) => {
+    return (
+      <use
+        xlinkHref={`#${member.type}`}
+        stroke={member.GetFace()}
+        stroke-width="0.75"
+        fill="none"
+        height={heightUse}
+        width={widthUse}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        x={PositionX(
+          member,
+          distance,
+          outerRadius,
+          innerRadius,
+          widthSvg,
+          widthUse,
+          angle0
+        )}
+        y={PositionY(
+          member,
+          distance,
+          outerRadius,
+          innerRadius,
+          heightSvg,
+          heightUse,
+          angle0
+        )}
+      />
+    );
+  };
 
   const View = (
     members,
@@ -153,6 +135,8 @@ const RaceView = (props) => {
     innerRadius,
     widthSvg,
     heightSvg,
+    widthUse,
+    heightUse,
     angle0
   ) => {
     return (
@@ -160,6 +144,7 @@ const RaceView = (props) => {
         <Row>
           <Col>
             <svg width={`${widthSvg}`} height={`${heightSvg}`}>
+              {GetDefs()}
               <g class="layer">
                 <circle
                   cx={`${CenterCoordinateX(widthSvg)}`}
@@ -179,34 +164,19 @@ const RaceView = (props) => {
                   stroke="#000000"
                   stroke-width="1"
                 />
-                {members.map((member) => (
-                  <circle
-                    cx={PositionX(
-                      member,
-                      distance,
-                      outerRadius,
-                      innerRadius,
-                      widthSvg,
-                      angle0
-                    )}
-                    cy={PositionY(
-                      member,
-                      distance,
-                      outerRadius,
-                      innerRadius,
-                      heightSvg,
-                      angle0
-                    )}
-                    fill={member.GetFace()}
-                    id="svg_5"
-                    r="10"
-                    stroke="null"
-                    stroke-dasharray="null"
-                    stroke-linecap="null"
-                    stroke-linejoin="null"
-                    stroke-width="null"
-                  />
-                ))}
+                {members.map((member) =>
+                  GetUse(
+                    member,
+                    distance,
+                    outerRadius,
+                    innerRadius,
+                    widthSvg,
+                    widthUse,
+                    heightSvg,
+                    heightUse,
+                    angle0
+                  )
+                )}
               </g>
             </svg>
           </Col>
@@ -221,6 +191,8 @@ const RaceView = (props) => {
     innerRadius,
     widthSvg,
     heightSvg,
+    widthUse,
+    heightUse,
     angle0
   );
 };
