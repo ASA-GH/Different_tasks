e52b4cfbb26ed93a0fde0021f30cd898f4840e95
ASA-GH <samorodov21@gmail.com>
2021-01-26 23:21:13 +0700

    Created a new repository folder # 4 (kyc)

---
+++ 4/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col, Container } from "react-bootstrap";
+// import "./App.scss";
+
+function App () {
+
+// const App = ( props ) => {
+  // const{ members,
+  //   distance,
+  //   race,
+  //   show,
+  //   HandlerStart,
+  //   DeleteMember,
+  //   HandleClose,
+  //   AddMember} = props
+  return (
+    <Container className="mainContainer">
+          <Row>text</Row>
+          <Row>text</Row> 
+          <Row>text</Row>
+   </Container>
+  );
+};
+export default App;
